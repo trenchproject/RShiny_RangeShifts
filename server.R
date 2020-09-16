43532506e29bb04ad2947a1ec59b7ee5775faca8
Yutaro Sakairi <yutaro.sakairi@gmail.com>
2020-09-15 21:45:09 -0700

    added a new plot

--- server.R
+++ server.R
@@ -5,14 +5,15 @@
 # Server
 #
 
-library(shiny)
-library(cowplot)
 
-#
 rgb.palette <- colorRampPalette(c("red", "orange", "blue"), space = "rgb")
+df <- read.csv("rangeshift.csv")
+df$taxa <- NA
+df$taxa[5] <- "fish"
+df$taxa[6] <- "squid"
 
 # Do house  keeping
-dat=read.csv(paste(getwd(),"/Ex3_rangedata.csv",sep = ""))
+dat = read.csv("Ex3_rangedata.csv")
 
 # Define server logic to do filtering
 shinyServer(function(input, output) {
@@ -26,18 +27,34 @@ shinyServer(function(input, output) {
     dat %>% filter(Margin %in% input$margins.sel)
     })
   
-  
-  output$RangeShiftPlot <- renderPlot({
-    
-    ggplot(data=dataset(), aes_string(x=input$x, y = input$y, color=input$color))+
-      theme_bw()+
-      geom_point()+geom_smooth(method="lm",se=FALSE) 
-    #+scale_colour_gradientn(colours =rgb.palette(20))
-    
-    #  +ylab("Development Index")+
-    #  xlab(xlab.title)+labs(color="Mean season gdds")+
-    #  theme(legend.position = "bottom") + guides(alpha=FALSE)
-    
+  df_filter <- reactive({
+    filter(df, taxa %in% input$taxa)
   })
   
+  # output$RangeShiftPlot <- renderPlot({
+  #   
+  #   ggplot(data=dataset(), aes_string(x=input$x, y = input$y, color=input$color))+
+  #     theme_bw()+
+  #     geom_point() + geom_smooth(method="lm", se=FALSE) 
+  #   #+scale_colour_gradientn(colours =rgb.palette(20))
+  #   
+  #   #  +ylab("Development Index")+
+  #   #  xlab(xlab.title)+labs(color="Mean season gdds")+
+  #   #  theme(legend.position = "bottom") + guides(alpha=FALSE)
+  #   
+  # })
+  
+  output$RangeShift <- renderPlot({
+    latPlot <- ggplot(data = df, aes(x = obslat1, y = gamhlat1)) +
+      geom_point() + 
+      xlab("Thermal envelope shift (°N/yr)") + ylab("Taxon shift (°N/yr)") +
+      theme_bw(base_size = 16) + geom_smooth(method = "lm", se=FALSE) 
+    
+    depthPlot <- ggplot(data = df, aes(x = obsdepth1, y = gamhdepth1)) +
+      geom_point() + 
+      xlab("Thermal envelope shift (°N/yr)") + ylab("Taxon shift (°N/yr)") +
+      theme_bw(base_size = 16) + geom_smooth(method = "lm", se=FALSE)
+    
+    grid.arrange(latPlot, depthPlot, nrow=2)
+  })
 })
--- ui.R
+++ ui.R
@@ -1,56 +1,51 @@
-#
-#
-#
 #Range shifts
 #
 
 library(shiny)
 library(tidyverse)
+library(gridExtra)
 
 #specify choices
 taxa= c("aquatic","insects","birds","mammals")
-margins= c("Upper","Lower","Average")
+margins = c("Upper","Lower","Average")
 
-dataset <-read.csv(paste(getwd(),"/Ex3_rangedata.csv",sep = ""))
+dataset <- read.csv("Ex3_rangedata.csv")
 
 # Define UI 
-shinyUI(
-
-# Define UI
-fluidPage(  
+shinyUI <- fluidPage(  
   title = "Range shifts",
-  fluidRow(
-    column(12,
-           includeMarkdown("include.md")
-    )),
 
- hr(),
+  includeMarkdown("include.md"),
+  hr(),
   # Place the filter horizontally
- fluidRow(
-     column(4,selectInput('x', 'Predictor variable (X)', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')), selected='ObservedShift_km'),
-     column(4,selectInput('y', 'Response variable (y)', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')), selected='ExpectedShift_km'),
-     column(4,selectInput('color', 'Color coding', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')))
-  ),
- 
- #pick species and sites 
- fluidRow(
-   column(4,selectInput('taxa.sel', 'Select taxa to plot', choices= as.character(taxa), multiple=TRUE, selectize=FALSE, selected=taxa)),
-   column(4,selectInput('margins.sel', 'Select range margins to plot', choices= as.character(margins), multiple=TRUE, selectize=FALSE, selected=margins))
- ),
+ #  fluidRow(
+ #     column(4,selectInput('x', 'Predictor variable (X)', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')), selected='ObservedShift_km'),
+ #     column(4,selectInput('y', 'Response variable (y)', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')), selected='ExpectedShift_km'),
+ #     column(4,selectInput('color', 'Color coding', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')))
+ #  ),
+ # 
+ # #pick species and sites 
+ #  fluidRow(
+ #    column(4,selectInput('taxa.sel', 'Select taxa to plot', choices= as.character(taxa), multiple=TRUE, selectize=FALSE, selected=taxa)),
+ #    column(4,selectInput('margins.sel', 'Select range margins to plot', choices= as.character(margins), multiple=TRUE, selectize=FALSE, selected=margins))
+ #  ),
  
   # Show a plot of the generated distribution
-  mainPanel(
-    column(12,plotOutput(outputId="RangeShiftPlot", width="800px",height="600px")
-  )),
+  # plotOutput(outputId="RangeShiftPlot", width="800px",height="600px"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("taxa", "Select taxa", choices = c("All", "fish"))
+    ),
+    mainPanel(
+      plotOutput("RangeShift")
+    )
+  ),
 
- hr(),
- fluidRow(
-   column(12,
-          includeMarkdown("include2.md")
-   )),
+  
+  hr(),
+  
+  includeMarkdown("include2.md"),
+  
+  hr()
 
- hr()
-
-
-)
 )
