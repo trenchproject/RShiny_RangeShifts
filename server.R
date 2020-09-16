1b3b867729cddb74d4be8341ac8467c8af93a779
Abigail Meyer <ameyer@Lady-Grey.home>
2020-09-16 10:54:49 -0400

    Added 1-1 line. Edited UI. Updated taxa.

--- server.R
+++ server.R
@@ -18,43 +18,30 @@ dat = read.csv("Ex3_rangedata.csv")
 # Define server logic to do filtering
 shinyServer(function(input, output) {
   
-  dataset <- reactive({
-   
-    #restrict taxa
-    dat %>% filter(TaxaGroup %in% input$taxa.sel)
-    
-    #restrict margin
-    dat %>% filter(Margin %in% input$margins.sel)
-    })
-  
-  df_filter <- reactive({
-    filter(df, taxa %in% input$taxa)
-  })
-  
-  # output$RangeShiftPlot <- renderPlot({
+  # dataset <- reactive({       Need to change this to new dataset
+  #  
+  #   #restrict taxa
+  #   dat %>% filter(TaxaGroup %in% input$taxa.sel)
   #   
-  #   ggplot(data=dataset(), aes_string(x=input$x, y = input$y, color=input$color))+
-  #     theme_bw()+
-  #     geom_point() + geom_smooth(method="lm", se=FALSE) 
-  #   #+scale_colour_gradientn(colours =rgb.palette(20))
-  #   
-  #   #  +ylab("Development Index")+
-  #   #  xlab(xlab.title)+labs(color="Mean season gdds")+
-  #   #  theme(legend.position = "bottom") + guides(alpha=FALSE)
-  #   
+  #   #restrict margin
+  #   dat %>% filter(Margin %in% input$margins.sel)
+  #   })
+  # 
+  # df_filter <- reactive({
+  #   filter(df, taxa %in% input$taxa)
   # })
   
   output$RangeShift <- renderPlot({
     latPlot <- ggplot(data = df, aes(x = obslat1, y = gamhlat1)) +
       geom_point() + 
       xlab("Thermal envelope shift (°N/yr)") + ylab("Taxon shift (°N/yr)") +
-      theme_bw(base_size = 16) + geom_smooth(method = "lm", se=FALSE) 
+      theme_bw(base_size = 16) + geom_abline(intercept=0, slope=1, col='red') 
     
     depthPlot <- ggplot(data = df, aes(x = obsdepth1, y = gamhdepth1)) +
       geom_point() + 
       xlab("Thermal envelope shift (°N/yr)") + ylab("Taxon shift (°N/yr)") +
-      theme_bw(base_size = 16) + geom_smooth(method = "lm", se=FALSE)
+      theme_bw(base_size = 16) + geom_abline(intercept=0, slope=1, col='red')
     
-    grid.arrange(latPlot, depthPlot, nrow=2)
+    grid.arrange(latPlot, depthPlot, ncol=2)
   })
 })
--- ui.R
+++ ui.R
@@ -6,42 +6,28 @@ library(tidyverse)
 library(gridExtra)
 
 #specify choices
-taxa= c("aquatic","insects","birds","mammals")
-margins = c("Upper","Lower","Average")
-
-dataset <- read.csv("Ex3_rangedata.csv")
+taxa= c("All","Fish","Mammals","Invertebrates")
 
 # Define UI 
 shinyUI <- fluidPage(  
   title = "Range shifts",
 
   includeMarkdown("include.md"),
+  
   hr(),
-  # Place the filter horizontally
- #  fluidRow(
- #     column(4,selectInput('x', 'Predictor variable (X)', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')), selected='ObservedShift_km'),
- #     column(4,selectInput('y', 'Response variable (y)', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')), selected='ExpectedShift_km'),
- #     column(4,selectInput('color', 'Color coding', c('Number of Species'='Nspecies','Duration (Years)'='Duration_Years',	'Mean Observed Shift (km)'='ObservedShift_km',	'SE Observed Shift (km)'='SEObservedShift_km',	'Expected Shift (km)'='ExpectedShift_km',	'Temperature Change (C)'='TemperatureChange_C')))
- #  ),
- # 
- # #pick species and sites 
- #  fluidRow(
- #    column(4,selectInput('taxa.sel', 'Select taxa to plot', choices= as.character(taxa), multiple=TRUE, selectize=FALSE, selected=taxa)),
- #    column(4,selectInput('margins.sel', 'Select range margins to plot', choices= as.character(margins), multiple=TRUE, selectize=FALSE, selected=margins))
- #  ),
  
-  # Show a plot of the generated distribution
-  # plotOutput(outputId="RangeShiftPlot", width="800px",height="600px"),
+  # Visualization of expected vs observed range shifts
   sidebarLayout(
     sidebarPanel(
-      selectInput("taxa", "Select taxa", choices = c("All", "fish"))
+      selectInput("taxa", "Select taxa", choices = taxa),
+      width=3
     ),
     mainPanel(
-      plotOutput("RangeShift")
+      plotOutput("RangeShift"),
+      width=9
     )
   ),
 
-  
   hr(),
   
   includeMarkdown("include2.md"),
